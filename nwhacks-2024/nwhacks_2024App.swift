58361d0a6f319a73641f0ce699399f2cfb6eb46c
Cindy Cui <misscindycui@gmail.com>
2024-01-20 11:01:05 -0800

    Initial Commit

---
+++ nwhacks-2024/nwhacks_2024App.swift
@@ -0,0 +1,32 @@
+//
+//  nwhacks_2024App.swift
+//  nwhacks-2024
+//
+//  Created by Cindy Cui on 2024-01-20.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct nwhacks_2024App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
